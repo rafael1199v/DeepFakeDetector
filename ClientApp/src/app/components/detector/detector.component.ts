a8ec1e3024d7045c13682b9acfdd0fed919f91ac
rafael1199v <123646379+rafael1199v@users.noreply.github.com>
2025-03-27 10:37:26 -0400

    Componente grafico funcional

--- ClientApp/src/app/components/detector/detector.component.ts
+++ ClientApp/src/app/components/detector/detector.component.ts
@@ -2,12 +2,13 @@ import { AfterViewInit, Component } from '@angular/core';
 import { ImageService } from '../../services/image.service';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { WebSnapshotComponent } from '../web-snapshot/web-snapshot.component';
+import { DonutChartComponent } from '../donut-chart/donut-chart.component';
 
 
 @Component({
   selector: 'app-detector',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, DonutChartComponent],
   providers: [ImageService],
   templateUrl: './detector.component.html',
   styleUrl: './detector.component.css'
@@ -17,6 +18,7 @@ export class DetectorComponent implements AfterViewInit{
   selectedFile: File | null = null;
   existsResult: boolean = false;
   resultClassification: any = null;
+  charClassification: any[] = [];
 
   constructor(private imageService: ImageService, private formBuilder: FormBuilder) {
   }
@@ -41,15 +43,29 @@ export class DetectorComponent implements AfterViewInit{
     formData.append('Image', this.selectedFile);
 
     this.imageService.classifyImage(formData).subscribe(
-     (result) => {
-      this.existsResult = true;
-      this.resultClassification = result;
-      console.log("Resultado de la clasificacion", this.resultClassification);
-     },
-     (error) => {
-      console.error("Error en la clasificacion de la imagen", error);
-     }
-    )
+      (result) => {
+        this.existsResult = true;
+        this.resultClassification = result;
+  
+        if (this.resultClassification.classificationResult[0].class == "Fake") {
+          this.charClassification = [
+            this.resultClassification.classificationResult[0],
+            this.resultClassification.classificationResult[1]
+          ];
+        } else {
+          this.charClassification = [
+            this.resultClassification.classificationResult[1],
+            this.resultClassification.classificationResult[0]
+          ];
+        }
+  
+        console.log("Resultado de la clasificación", this.resultClassification);
+      },
+      (error) => {
+        console.error("Error en la clasificación de la imagen", error);
+      }
+    );
+  
 
   }
 
--- ClientApp/src/app/components/donut-chart/donut-chart.component.ts
+++ ClientApp/src/app/components/donut-chart/donut-chart.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Chart } from 'chart.js/auto';
 
 @Component({
   selector: 'app-donut-chart',
@@ -7,9 +8,45 @@ import { Component } from '@angular/core';
   templateUrl: './donut-chart.component.html',
   styleUrl: './donut-chart.component.css'
 })
-export class DonutChartComponent {
+export class DonutChartComponent implements OnChanges{
 
-  ngOnInit() : void {
-    
+  chart: any;
+  @Input() fakePercentage: number = 0;
+  @Input() realPercentage: number = 0;
+
+ 
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes['fakePercentage'] || changes['realPercentage']) {
+      this.updateChart();
+    }
+  }
+
+  initChart() : void {
+    this.fakePercentage *= 100;
+    this.realPercentage *= 100;
+
+    this.chart = new Chart('canvas', {
+      type: 'doughnut',
+      data: {
+        labels: ["Fake", "Real"],
+        datasets: [
+          {
+            data: [this.fakePercentage, this.realPercentage],
+            backgroundColor: ['rgba(216, 20, 20, 1)', 'rgba(13, 108, 17, 1)',],
+          },
+        ]
+      }
+    })
+  }
+
+
+  updateChart() : void {
+    if(this.chart) {
+      this.chart.data.datasets[0].data = [this.fakePercentage * 100, this.realPercentage * 100]
+      this.chart.update();
+    }
+    else {
+      this.initChart();
+    }
   }
 }
